0fd60d190d17bddad3ae801bb738a9c055f3621b
Dennis Evangelista <evangeli@usna.edu>
2020-04-28 11:49:53 -0400

    Cleaning up the test

--- data/drag-analysis.R
+++ data/drag-analysis.R
@@ -20,21 +20,20 @@ data <- mutate(data,
 
 # do analysis of variance (ANOVA)
 foo <- aov(D.N~as.factor(speed)+as.factor(leaf),data)
-summary(foo)
-#                 Df   Sum Sq  Mean Sq F value   Pr(>F)    
-#as.factor(speed)  2 0.000260 0.000130   1.087    0.361    
-#as.factor(leaf)   1 0.004387 0.004387  36.660 1.67e-05 ***
-#Residuals        16 0.001915 0.000120                     
+#summary(foo)
+#                 Df  Sum Sq  Mean Sq F value   Pr(>F)    
+#as.factor(speed)  2 0.00066 0.000330   5.674   0.0137 *  
+#as.factor(leaf)   1 0.01028 0.010277 176.796 4.59e-10 ***
+#Residuals        16 0.00093 0.000058                     
 #---
 #Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
 
 bar <- aov(DA.Nm2~as.factor(speed)+as.factor(leaf),data)
-#summary(bar)
-#                 Df Sum Sq Mean Sq F value   Pr(>F)    
-#as.factor(speed)  2  119.1   59.54   8.474 0.003093 ** 
-#as.factor(leaf)   1  142.7  142.66  20.303 0.000359 ***
-#Residuals        16  112.4    7.03                     
+#                 Df Sum Sq Mean Sq F value Pr(>F)  
+#as.factor(speed)  2 10.972   5.486   4.653 0.0255 *
+#as.factor(leaf)   1  1.238   1.238   1.050 0.3207  
+#Residuals        16 18.864   1.179                 
 #---
 #Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
@@ -51,10 +50,10 @@ drag.plot <- ggplot(drag.summary, aes(x=as.factor(speed),y=mean.D,fill=leaf))+
     geom_col(position=position_dodge2(preserve="single"))+
     geom_errorbar(aes(ymin=mean.D-sd.D,ymax=mean.D+sd.D),width=0.2,
                   position=position_dodge(width=0.9))+
-    xlab(TeX("fan setting"))+
-    ylab(TeX("drag, N"))+
+    xlab("fan setting")+
+    ylab("drag, N")+
     scale_fill_manual(values=c("green","gray50"))+
-    annotate("text",x=3.23,y=0.0625,label="*")+
+    annotate("text",x=3.23,y=0.025,label="*")+
     theme_bw(base_size=8)+
     theme(legend.position="none")
 pdf('results1.pdf',width=3,height=2)
@@ -65,12 +64,11 @@ da.plot <- ggplot(drag.summary, aes(x=as.factor(speed),y=mean.DA,fill=leaf))+
     geom_col(position=position_dodge2(preserve="single"))+
     geom_errorbar(aes(ymin=mean.DA-sd.DA,ymax=mean.DA+sd.DA),width=0.2,
                   position=position_dodge(width=0.9))+
-    xlab(TeX("fan setting"))+
-    ylab(TeX("drag/area, N/m$^2$"))+
+    xlab("fan setting")+
+    ylab("drag/area, N/m$^2$")+
     scale_fill_manual(values=c("green","gray50"))+
-    annotate("segment",x=1-0.2,xend=2+0.2,y=17,yend=17)+
-    annotate("text",x=2.77,y=17,label="*")+
-    annotate("text",x=3.23,y=17,label="**")+
+    annotate("segment",x=1-0.2,xend=2+0.2,y=7,yend=7)+
+    annotate("segment",x=2-0.2,xend=3,y=7.25,yend=7.25)+
     theme_bw(base_size=8)+
     theme(legend.position="none")
 pdf('results2.pdf',width=3,height=2)
@@ -83,3 +81,16 @@ dev.off()
 
 ggsave(file="results1.svg",plot=drag.plot,width=3,height=2);
 ggsave(file="results2.svg",plot=da.plot,width=3,height=2);
+
+
+
+
+# get some group stats
+grouped <- group_by(data, speed, leaf)
+grouped.results <- summarise(grouped,
+                             mean.D=mean(D.N),
+                             sd.D=sd(D.N),
+                             mean.DA=mean(DA.Nm2),
+                             sd.DA=sd(DA.Nm2))
+
+                         
